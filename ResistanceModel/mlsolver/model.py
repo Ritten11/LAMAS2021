331c873ac479dffa916fd6f7c96e5e556f2c1b38
EKCollins <e.k.collins@student.rug.nl>
2021-06-11 12:35:48 +0200

    mlsolver

---
+++ ResistanceModel/mlsolver/model.py
@@ -0,0 +1,108 @@
+""" Three wise men puzzle
+
+Module contains data model for three wise men puzzle as Kripke strukture and agents announcements as modal logic
+formulas
+"""
+
+from mlsolver.kripke import KripkeStructure, World
+from mlsolver.formula import Atom, And, Not, Or, Box_a, Box_star
+
+
+class WiseMenWithHat:
+    """
+    Class models the Kripke structure of the "Three wise men example.
+    """
+
+    knowledge_base = []
+
+    def __init__(self):
+        worlds = [
+            World('RWW', {'1:R': True, '2:W': True, '3:W': True}),
+            World('RRW', {'1:R': True, '2:R': True, '3:W': True}),
+            World('RRR', {'1:R': True, '2:R': True, '3:R': True}),
+            World('WRR', {'1:W': True, '2:R': True, '3:R': True}),
+
+            World('WWR', {'1:W': True, '2:W': True, '3:R': True}),
+            World('RWR', {'1:R': True, '2:W': True, '3:R': True}),
+            World('WRW', {'1:W': True, '2:R': True, '3:W': True}),
+            World('WWW', {'1:W': True, '2:W': True, '3:W': True}),
+        ]
+
+        relations = {
+            '1': {('RWW', 'WWW'), ('RRW', 'WRW'), ('RWR', 'WWR'), ('WRR', 'RRR')},
+            '2': {('RWR', 'RRR'), ('RWW', 'RRW'), ('WRR', 'WWR'), ('WWW', 'WRW')},
+            '3': {('WWR', 'WWW'), ('RRR', 'RRW'), ('RWW', 'RWR'), ('WRW', 'WRR')}
+        }
+
+        relations.update(add_reflexive_edges(worlds, relations))
+        relations.update(add_symmetric_edges(relations))
+
+        self.ks = KripkeStructure(worlds, relations)
+
+        # Wise man ONE does not know whether he wears a red hat or not
+        self.knowledge_base.append(And(Not(Box_a('1', Atom('1:R'))), Not(Box_a('1', Not(Atom('1:R'))))))
+
+        # This announcement implies that either second or third wise man wears a red hat.
+        self.knowledge_base.append(Box_star(Or(Atom('2:R'), Atom('3:R'))))
+
+        # Wise man TWO does not know whether he wears a red hat or not
+        self.knowledge_base.append(And(Not(Box_a('2', Atom('2:R'))), Not(Box_a('2', Not(Atom('2:R'))))))
+
+        # This announcement implies that third men has be the one, who wears a red hat
+        self.knowledge_base.append(Box_a('3', Atom('3:R')))
+
+class Resistance3Agents:
+    """
+    This class models the Kripke structure of the Resistance game with 3 agents.
+    """
+    knowledge_base = []
+
+    def __init__(self):
+        worlds = [
+            World('s1', {'s1': True, 's2': False, 's3': False}),
+            World('s2', {'s1': False, 's2': True, 's3': False}),
+            World('s3', {'s1': False, 's2': False, 's3': True})
+            ]
+        relations = {
+            '1': {('s2', 's3'), ('s3', 's2')},
+            '2': {('s1', 's3'), ('s3', 's1')},
+            '3': {('s1', 's2'), ('s2', 's1')}
+        }
+        relations.update(add_reflexive_edges(worlds, relations))
+        self.ks = KripkeStructure(worlds, relations)
+
+class Resistance5Agents: 
+    """
+    This class models the Kripke structure of the Resistance game with 5 agents.
+    """
+    knowledge_base = []
+
+    def __init__(self):
+        worlds = []
+
+        relations = {}
+
+
+def add_symmetric_edges(relations):
+    """Routine adds symmetric edges to Kripke frame
+    """
+    result = {}
+    for agent, agents_relations in relations.items():
+        result_agents = agents_relations.copy()
+        for r in agents_relations:
+            x, y = r[1], r[0]
+            result_agents.add((x, y))
+        result[agent] = result_agents
+    return result
+
+
+def add_reflexive_edges(worlds, relations):
+    """Routine adds reflexive edges to Kripke frame
+    """
+    result = {}
+    for agent, agents_relations in relations.items():
+        result_agents = agents_relations.copy()
+        for world in worlds:
+            result_agents.add((world.name, world.name))
+            result[agent] = result_agents
+    return result
