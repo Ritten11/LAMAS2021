21794fa5ec9b830fa10c58c2b4d23aba33a672c7
Lonneke Langeveld <l.langeveld@gmail.com>
2021-06-24 14:17:39 +0200

    Add some comments and check pep8-compliance

--- ResistanceModel/Resistance.py
+++ ResistanceModel/Resistance.py
@@ -2,118 +2,127 @@ from ResistanceModel.AbstractAgent import AbstractAgent
 from ResistanceModel.mlsolver.formula import Atom, And, Not, Or, Box_a, Box_star
 import random
 
+
 class Resistance(AbstractAgent):
-	def __init__(self,unique_id, model):
-		super().__init__(unique_id, model)
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
 
-	def initKB(self): # I dont think we need this right? we can just initialize this in __int__?
-		self.card = None
-		self.vote = None
+    def initKB(self):  # I dont think we need this right? we can just initialize this in __int__?
+        self.card = None
+        self.vote = None
 
-	def step(self):
-		'''
-		Advances the model by one step using flags.
-		choose_team: if the agent is the mission leader they get to choose the team
-		vote: the agent gets to vote on the chosen team
-		go_on_mission: if the agent is on the mission they choose a card to play
-		update_knowledge: the agent deliberates on the voting results of the mission
-			based on the result of the mission
-		'''
-		if self.model.state == "choose_team":
-			if self.model.mission_leader == self.unique_id:
-				self.model.grid.move_agent(self, (self.unique_id, 2))
-				self.model.mission_team = self.choose_team()
-				if self.model.debugging:
-					print(f"team is {self.model.mission_team}")
-			else:
-				self.model.grid.move_agent(self, (self.unique_id, 0))
+    def step(self):
+        '''
+        Advances the model by one step using flags.
 
-		if self.model.state == "vote":
-			if self.decide_on_vote():
-				self.vote = "Yes"
-			else:
-				self.vote = "No"
-			if self.model.debugging:
-				print(f"Agent {self.unique_id} voted {self.vote}")
+        choose_team: if the agent is the mission leader they get to choose the team
+        vote: the agent gets to vote on the chosen team
+        go_on_mission: if the agent is on the mission they choose a card to play
+        update_knowledge: the agent deliberates on the voting results of the mission
+            based on the result of the mission
+        '''
 
-		if self.model.state == "go_on_mission":
-			if self.unique_id in self.model.mission_team:
-				self.model.grid.move_agent(self, (self.unique_id, 4))
+        # State in which a team is chosen
+        if self.model.state == "choose_team":
+            if self.model.mission_leader == self.unique_id:
+                self.model.grid.move_agent(self, (self.unique_id, 2))
+                self.model.mission_team = self.choose_team()
+                if self.model.debugging:
+                    print(f"team is {self.model.mission_team}")
+            else:
+                self.model.grid.move_agent(self, (self.unique_id, 0))
 
-		if self.model.state == "play":
-			if self.unique_id in self.model.mission_team:
-				self.card = "Pass" # resistance agents always play a "pass" card
-				if self.model.debugging:
-					print(f"{self.card} card is played by {self.unique_id}")
-		   
-		if self.model.state == "update_knowledge":
-			self.model.grid.move_agent(self, (self.unique_id, 0))
-			if self.model.resistance_reasons:
-				self.updateKB()
+        # State in which agents vote on whether to accept a mission team
+        if self.model.state == "vote":
+            if self.decide_on_vote():
+                self.vote = "Yes"
+            else:
+                self.vote = "No"
+            if self.model.debugging:
+                print(f"Agent {self.unique_id} voted {self.vote}")
 
-	def choose_team(self):
-		'''
-		This function is the resistance agent's reasoning for choosing a team.
-		We first go through the agents to see whether the agent knows the spies. If the agent
-		knows the identity of a spy, they will decide not to choose them for the mission. The
-		agent will always trust themselves and will fill the rest of the mission team randomly.
-		:return: the chosen mission team
-		'''
-		mission_team = []
-		dont_choose = []
+        # State in which agents go on mission (for GUI)
+        if self.model.state == "go_on_mission":
+            if self.unique_id in self.model.mission_team:
+                self.model.grid.move_agent(self, (self.unique_id, 4))
 
-		for agent in range(1, self.model.num_agents+1):
-			formula = And(Box_a(str(self.unique_id), Atom(str(agent))), Not(Atom(str(self.unique_id))))
-			nodes = self.model.kripke_model.ks.nodes_not_follow_formula(formula)
-			if len(nodes) < len(self.model.kripke_model.ks.worlds):
-				dont_choose.append(agent)
+        # State in which the cards are played
+        if self.model.state == "play":
+            if self.unique_id in self.model.mission_team:
+                self.card = "Pass"  # resistance agents always play a "pass" card
+                if self.model.debugging:
+                    print(f"{self.card} card is played by {self.unique_id}")
 
-		mission_team.append(self.unique_id) # agent always trusts themselves
-		while len(mission_team) != self.model.team_sizes[self.model.mission_number - 1]:
-			temp = random.choice(range(1, self.model.num_agents+1))
-			if temp not in mission_team and temp not in dont_choose:
-				mission_team.append(temp)
-		return mission_team
+        # After each mission, an agent moves back in the GUI.
+        # If reasoning is enabled, an agent reasons about the vote.
+        if self.model.state == "update_knowledge":
+            self.model.grid.move_agent(self, (self.unique_id, 0))
+            if self.model.resistance_reasons:
+                self.updateKB()
 
-	def decide_on_vote(self):
-		'''
-		
-		:return: 
-		'''
-		# Checks whether the agent accepts the mission team or not
-		relations = self.model.kripke_model.ks.relations[str(self.unique_id)]
-		worlds = [rel[1] for rel in relations if rel[0] == self.model.true_world]
-		suspicions = [0] * len(self.model.mission_team)
-		for i, agent in enumerate(self.model.mission_team):
-			if agent == self.unique_id:
-				continue
-			else:
-				for world in worlds:
-					if str(agent) in world:
-						suspicions[i] += 1
-		for s in suspicions:
-			if s == len(worlds):
-				return False
+    def choose_team(self):
+        '''
+        This function is the resistance agent's reasoning for choosing a team.
+        We first go through the agents to see whether the agent knows the spies. If the agent
+        knows the identity of a spy, they will decide not to choose them for the mission. The
+        agent will always trust themselves and will fill the rest of the mission team randomly.
+        :return: the chosen mission team
+        '''
+        mission_team = []
+        dont_choose = []
 
-		return True
+        for agent in range(1, self.model.num_agents+1):
+            formula = And(Box_a(str(self.unique_id), Atom(str(agent))), Not(Atom(str(self.unique_id))))
+            nodes = self.model.kripke_model.ks.nodes_not_follow_formula(formula)
+            if len(nodes) < len(self.model.kripke_model.ks.worlds):
+                dont_choose.append(agent)
 
-	def updateKB(self):
-		'''
-		This function helps the agent learn from the votes of agents for the mission.
-		If an agent voted for a mission that failed, then that agent may be a spy.
-		'''
-		print("The resistance is now using higher order knowledge")
-		if self.model.failed: 
-			yes_votes = []
-			for agent in self.model.schedule.agents:
-				if agent.vote == "Yes":
-					yes_votes.append(agent.unique_id)
-			print(yes_votes)
-			temp = [Atom(str(a)) for a in yes_votes]
-			formula = Or(Or(temp[0], temp[1]), temp[2])
-			if len(yes_votes) == 4: 
-				formula = Or(formula, temp[3])
-			if len(yes_votes) == 5:
-				formula = Or(Or(formula, temp[3]), temp[4])
-			print(formula)
-			self.model.kripke_model.ks = self.model.kripke_model.ks.solve(formula)
+        mission_team.append(self.unique_id)  # agents always trusts themselves
+        while len(mission_team) != self.model.team_sizes[self.model.mission_number - 1]:
+            temp = random.choice(range(1, self.model.num_agents+1))
+            if temp not in mission_team and temp not in dont_choose:
+                mission_team.append(temp)
+        return mission_team
+
+    def decide_on_vote(self):
+        """Checks whether a Resistance agent accepts the proposed mission team"""
+        # Find relations for the current agent
+        relations = self.model.kripke_model.ks.relations[str(self.unique_id)]
+        # Find relations to other worlds from the true world
+        worlds = [rel[1] for rel in relations if rel[0] == self.model.true_world]
+        suspicions = [0] * len(self.model.mission_team)
+        for i, agent in enumerate(self.model.mission_team):
+            if agent == self.unique_id:
+                continue
+            else:
+                for world in worlds:
+                    if str(agent) in world:
+                        suspicions[i] += 1
+
+        # Vote no if an agent is sure a spy is in the mission team
+        for s in suspicions:
+            if s == len(worlds):
+                return False
+
+        return True
+
+    def updateKB(self):
+        """
+        This function helps the agent learn from the votes of agents for the mission.
+        If an agent voted for a mission that failed, then that agent may be a spy.
+        """
+        print("The resistance is now using higher order knowledge")
+        if self.model.failed:
+            yes_votes = []
+            for agent in self.model.schedule.agents:
+                if agent.vote == "Yes":
+                    yes_votes.append(agent.unique_id)
+            print(yes_votes)
+            temp = [Atom(str(a)) for a in yes_votes]
+            formula = Or(Or(temp[0], temp[1]), temp[2])
+            if len(yes_votes) == 4:
+                formula = Or(formula, temp[3])
+            if len(yes_votes) == 5:
+                formula = Or(Or(formula, temp[3]), temp[4])
+            print(formula)
+            self.model.kripke_model.ks = self.model.kripke_model.ks.solve(formula)
