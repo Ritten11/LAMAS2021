effaede2e2c471aac4c0c7c3ac4ef3d46bcaf546
Lonneke Langeveld <l.langeveld@gmail.com>
2021-06-25 09:17:23 +0200

    Split tables up in two

--- results/other_results.py
+++ results/other_results.py
@@ -7,59 +7,60 @@ def read_data(n):
     return pd.read_json(f"N_{n}_output.json")
 
 
-def get_results(df1, df2):
-    s1 = [df1['sphok'].unique(), df1['rhok'].unique(), df1['ps'].unique()]
-    p1 = list(itertools.product(*s1))
-    df = pd.DataFrame(columns=['N', 'SPHOK', 'RHOK', 'PS', 'not revealed', 'average'])
-
-    for i, combo in enumerate(p1):
-        numbers = df1.query(f'sphok == {combo[0]} and rhok == {combo[1]}'
-                              f' and ps == "{str(combo[2])}"')['identity_revealed']
-        no_reveal = 0
-        num = 0
-        den = 0
-        for n in numbers:
-            if n == 0:
-                no_reveal += 1
-            else:
-                num += n
-                den += 1
-        if den != 0:
-            avg = num / den
+def get_numbers(nums):
+    no_reveal = 0
+    num = 0
+    den = 0
+    for n in nums:
+        if n == 0:
+            no_reveal += 1
         else:
-            avg = 0
-        df = df.append({'N': 5, 'SPHOK': combo[0], 'RHOK': combo[1], 'PS': combo[2],
+            num += n
+            den += 1
+    if den != 0:
+        avg = num / den
+    else:
+        avg = 'NA'
+
+    return no_reveal, avg
+
+
+
+def get_results_5(df):
+    s =[df['sphok'].unique(), df['rhok'].unique(), df['ps'].unique()]
+    p = list(itertools.product(*s))
+    new_df = pd.DataFrame(columns=['SPHOK', 'RHOK', 'PS', 'not revealed', 'average'])
+    for i, combo in enumerate(p):
+        nums = df.query(f'sphok == {combo[0]} and rhok == {combo[1]}'
+                              f' and ps == "{str(combo[2])}"')['identity_revealed']
+        no_reveal, avg = get_numbers(nums)
+        new_df = new_df.append({'SPHOK': combo[0], 'RHOK': combo[1], 'PS': combo[2],
                         'not revealed': no_reveal, 'average':avg},ignore_index=True)
 
-    s2 = [df1['sphok'].unique(), df1['rhok'].unique()]
-    p2 = list(itertools.product(*s2))
-    for j, combo2 in enumerate(p2):
-        numbers = df2.query(f'sphok == {combo2[0]} and rhok == {combo2[1]}')['identity_revealed']
-        no_reveal = 0
-        num = 0
-        den = 0
-        for n in numbers:
-            if n == 0:
-                no_reveal += 1
-            else:
-                num += n
-                den += 1
-        if den != 0:
-            avg = num / den
-        else:
-            avg = 0
-        df = df.append({'N': 6, 'SPHOK': combo2[0], 'RHOK': combo2[1], 'PS': 'def',
-                        'not revealed': no_reveal, 'average':avg}, ignore_index=True)
+    return new_df
 
-    return df
+def get_results_6(df):
+    s = [df['sphok'].unique(), df['rhok'].unique()]
+    p = list(itertools.product(*s))
+    new_df = pd.DataFrame(columns=['SPHOK', 'RHOK', 'not revealed', 'average'])
+    for i, combo in enumerate(p):
+        nums = df.query(f'sphok == {combo[0]} and rhok == {combo[1]}')['identity_revealed']
+        no_reveal, avg = get_numbers(nums)
+        new_df = new_df.append({'SPHOK': combo[0], 'RHOK': combo[1],
+                                'not revealed': no_reveal, 'average': avg}, ignore_index=True)
+
+    return new_df
 
 
 def main():
     n5 = read_data(5)
     n6 = read_data(6)
-    results = get_results(n5, n6)
+    results5 = get_results_5(n5)
+    results6 = get_results_6(n6)
     with open('identity_revealed_results.txt', 'w') as f:
-        f.write(results.to_markdown(index=False))
+        f.write(results5.to_markdown(index=False))
+        f.write("\n\n")
+        f.write(results6.to_markdown(index=False))
 
 if __name__ == "__main__":
     main()
